9c9377678d873cdcb594ede3f05da3285ec736ec
Raul Gomez <raul.gomezc@softtek.com>
2025-05-04 18:05:08 +0200

    connection to Google API

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rweather/models/hourly_weather.dart';
 import 'package:rweather/models/weather_info.dart';
 import 'package:rweather/services/background_image_service.dart';
+import 'package:rweather/services/geocoding_service.dart';
 import 'package:rweather/services/weather_service.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,6 +16,8 @@ class HomeScreen extends StatefulWidget {
 class HomeScreenState extends State<HomeScreen> {
   final weatherService = WeatherService();
   final backgroundImageService = BackgroundImageService();
+  final cityService = CitySearchService();
+  final TextEditingController cityController = TextEditingController();
 
   double temperature = 0;
   double minTemperature = 0;
@@ -25,7 +29,7 @@ class HomeScreenState extends State<HomeScreen> {
   WeatherInfo? weatherData;
   List<HourlyWeather> hourlyForecast = [];
   String? backgroundImageUrl;
-
+  bool searchingCity = false;
 
   @override
   void initState() {
@@ -46,7 +50,8 @@ class HomeScreenState extends State<HomeScreen> {
         description = weatherData!.description;
         feelsLike = weatherData!.feelsLike;
         windSpeed = weatherData!.windSpeed;
-        loadBackgroundImage(weatherData!.description); // Cambiar el fondo según el clima
+        loadBackgroundImage(
+            weatherData!.description); // Cambiar el fondo según el clima
       });
     } catch (e) {
       print(e);
@@ -54,16 +59,15 @@ class HomeScreenState extends State<HomeScreen> {
   }
 
   Future<void> loadBackgroundImage(String weatherType) async {
-  try {
-    final url = await backgroundImageService.getImageUrl(weatherType);
-    setState(() {
-      backgroundImageUrl = url;
-    });
-  } catch (e) {
-    print('Error al cargar la imagen de fondo: $e');
+    try {
+      final url = await backgroundImageService.getImageUrl(weatherType);
+      setState(() {
+        backgroundImageUrl = url;
+      });
+    } catch (e) {
+      print('Error al cargar la imagen de fondo: $e');
+    }
   }
-}
-
 
   Future<void> loadHourlyForecast() async {
     try {
@@ -83,19 +87,15 @@ class HomeScreenState extends State<HomeScreen> {
     // String backgroundImageUrl = backgroundImageService.getImageUrl(weatherType);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HOME'),
-        centerTitle: true,
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
-        ],
-      ),
       body: weatherData == null || backgroundImageUrl == null
           ? const Center(child: CircularProgressIndicator())
           : RefreshIndicator(
               onRefresh: () async {
                 await loadWeather();
                 await loadHourlyForecast();
+                setState(() {
+                  searchingCity = false;
+                });
               },
               child: SingleChildScrollView(
                 physics: const AlwaysScrollableScrollPhysics(),
@@ -109,100 +109,154 @@ class HomeScreenState extends State<HomeScreen> {
                     ),
                     color: const Color.fromARGB(255, 9, 120, 135).withAlpha(55),
                   ),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          TextButton(
-                              onPressed: () {}, child: const Text('Ayer')),
-                          TextButton(
-                              onPressed: () {}, child: const Text('Hoy')),
-                          TextButton(
-                              onPressed: () {}, child: const Text('Mañana'))
-                        ],
-                      ),
-                      Image.network(
-                        'https://openweathermap.org/img/wn/${weatherData!.icon}@2x.png',
-                        width: 100,
-                        height: 100,
-                      ),
-                      Text(weatherData!.description),
-                      Text(city),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Icon(Icons.wind_power),
-                              Text('$windSpeed m/s')
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              Text('$temperatureºC'),
-                              Text('Sensación de $feelsLikeºC')
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              Text('Min. $minTemperature'),
-                              Text('Max. $maxTemperature')
-                            ],
-                          )
-                        ],
-                      ),
-                      hourlyForecast.isEmpty
-                          ? const Text('Fallo en predicción')
-                          : SizedBox(
-                              height: 140,
-                              child: ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                itemCount: hourlyForecast.length,
-                                itemBuilder: (context, index) {
-                                  final item = hourlyForecast[index];
-                                  final hour =
-                                      TimeOfDay.fromDateTime(item.dateTime)
-                                          .format(context);
-                                  final rainChance = (item.probOfRain * 100)
-                                      .toStringAsFixed(0);
-
-                                  return Card(
-                                    color: const Color.fromARGB(255, 0, 0, 0)
-                                        .withAlpha(0),
-                                    margin: const EdgeInsets.all(8),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            hour,
-                                            style: const TextStyle(
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                          Image.network(
-                                            'https://openweathermap.org/img/wn/${item.icon}@2x.png',
-                                            width: 50,
-                                            height: 50,
-                                          ),
-                                          Text('${item.temperature}ºC'),
-                                          Text('$rainChance% lluvia',
-                                              style: const TextStyle(
-                                                  fontSize: 12)),
-                                        ],
-                                      ),
-                                    ),
-                                  );
-                                },
+                  child: SafeArea(
+                    child: Column(
+                      children: [
+                        if (searchingCity)
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 12),
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 0, 0, 0)
+                                      .withAlpha(0),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            width: MediaQuery.of(context).size.width / 2,
+                            child: TextField(
+                              controller: cityController,
+                              style: const TextStyle(color: Colors.white70),
+                              decoration: const InputDecoration(
+                                isDense: true,
+                                hintText: 'Ingresa una ciudad',
+                                hintStyle: TextStyle(color: Colors.white70),
+                                border: InputBorder
+                                    .none, // Eliminamos el borde nativo
                               ),
                             ),
-                    ],
+                          )
+                        else
+                          ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                searchingCity = true;
+                                cityService.obtenerCoordenadas('Guadalajara');
+                              });
+                            },
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 12),
+                              backgroundColor:
+                                  const Color.fromARGB(255, 0, 0, 0)
+                                      .withAlpha(0),
+                            ),
+                            child: const Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(
+                                  Icons.search,
+                                  color: Colors.white70,
+                                ),
+                                SizedBox(width: 8),
+                                Text(
+                                  'Buscar ciudad',
+                                  style: TextStyle(color: Colors.white70),
+                                ),
+                              ],
+                            ),
+                          ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            TextButton(
+                                onPressed: () {}, child: const Text('Ayer')),
+                            TextButton(
+                                onPressed: () {}, child: const Text('Hoy')),
+                            TextButton(
+                                onPressed: () {}, child: const Text('Mañana'))
+                          ],
+                        ),
+                        Image.network(
+                          'https://openweathermap.org/img/wn/${weatherData!.icon}@2x.png',
+                          width: 100,
+                          height: 100,
+                        ),
+                        Text(weatherData!.description),
+                        Text(city),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(Icons.wind_power),
+                                Text('$windSpeed m/s')
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text('$temperatureºC'),
+                                Text('Sensación de $feelsLikeºC')
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text('Min. $minTemperature'),
+                                Text('Max. $maxTemperature')
+                              ],
+                            )
+                          ],
+                        ),
+                        hourlyForecast.isEmpty
+                            ? const Text('Fallo en predicción')
+                            : SizedBox(
+                                height: 140,
+                                child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: hourlyForecast.length,
+                                  itemBuilder: (context, index) {
+                                    final item = hourlyForecast[index];
+                                    final hour =
+                                        TimeOfDay.fromDateTime(item.dateTime)
+                                            .format(context);
+                                    final rainChance = (item.probOfRain * 100)
+                                        .toStringAsFixed(0);
+
+                                    return Card(
+                                      color: const Color.fromARGB(255, 0, 0, 0)
+                                          .withAlpha(0),
+                                      margin: const EdgeInsets.all(8),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              hour,
+                                              style: const TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            Image.network(
+                                              'https://openweathermap.org/img/wn/${item.icon}@2x.png',
+                                              width: 50,
+                                              height: 50,
+                                            ),
+                                            Text('${item.temperature}ºC'),
+                                            Text('$rainChance% lluvia',
+                                                style: const TextStyle(
+                                                    fontSize: 12)),
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                      ],
+                    ),
                   ),
                 ),
               ),
---
+++ lib/services/geocoding_service.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class CitySearchService {
+  final String? apiKey = dotenv.env['MAPS_KEY'];
+
+  /// Buscar sugerencias de ciudades usando Google Places Autocomplete API
+  Future<List<Map<String, String>>> buscarCiudades(String input) async {
+    if (apiKey == null) throw Exception('API Key no encontrada');
+
+    final uri = Uri.parse(
+      'https://maps.googleapis.com/maps/api/place/autocomplete/json'
+      '?input=$input'
+      '&types=(cities)'
+      '&language=es'
+      '&key=$apiKey',
+    );
+
+    final response = await http.get(uri);
+    final data = json.decode(response.body);
+
+    if (data['status'] == 'OK') {
+      return List<Map<String, String>>.from(data['predictions'].map((p) => {
+            'description': p['description'],
+            'place_id': p['place_id'],
+          }));
+    } else {
+      throw Exception('Error al buscar ciudades: ${data['status']}');
+    }
+  }
+
+  /// Obtener coordenadas de una ciudad usando Place Details API
+  // Future<Map<String, double>> obtenerCoordenadas(String placeId) async {
+  void obtenerCoordenadas(String place) async {
+    if (apiKey == null) throw Exception('API Key no encontrada');
+
+    final uri = Uri.parse(
+      'https://maps.googleapis.com/maps/api/geocode/json?'
+      'address=$place'
+      '&key=$apiKey',
+    );
+
+    final response = await http.get(uri);
+    final data = json.decode(response.body);
+
+    if (data['status'] == 'OK') {
+      final location = data['results'][0]['geometry']['location'];
+      print(location);
+      // return {
+      //   'lat': location['lat'],
+      //   'lng': location['lng'],
+      // };
+    } else {
+      throw Exception('Error al obtener coordenadas: ${data['status']}');
+    }
+  }
+}
